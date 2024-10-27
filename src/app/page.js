3920ab35cad00d6741e939af29447c39d3ec7946
“kaufast” <kaufast@gmail.com>
2024-10-27 07:08:00 +0100

    nextauth added

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,19 @@
 import Home_V1 from "./(home)/home-v1/page";
 import Wrapper from "./layout-wrapper/wrapper";
-import Head from 'next/head'; // Import Head component
 
-
+// Define metadata object
 export const metadata = {
   title: "Inmobi v1 || Inmobiliaria - Real Estate",
+  robots: {
+    index: false,
+    follow: false
+  }
 };
-
 
 export default function MainRoot() {
   return (
-    <>
-      <Head>
-        <meta name="robots" content="noindex, nofollow" />
-      </Head>
-      <Wrapper>
-        <Home_V1 />
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Home_V1 />
+    </Wrapper>
   );
 }
