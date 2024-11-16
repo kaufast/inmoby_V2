c16b13acdbafa7aa72b4a97b8ee414c1cfcaf13e
“kaufast” <kaufast@gmail.com>
2024-11-16 10:04:58 +0100

    translations working fine

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 
-module.exports = {
-
-  };
-  
+  module.exports = {
+  // i18n: {
+  //   locales: ['en', 'es'], // Locale slugs
+  //   defaultLocale: 'es',   // Default locale
+  //   localeDetection: false // Optional, disables auto-detection
+  // },
+  // middleware: ['/'],
+};
